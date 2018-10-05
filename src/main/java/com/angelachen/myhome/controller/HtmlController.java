3802802da80fda62c5c1cc651c8e8a2bf5bffae8
kartist <297754656@qq.com>
2018-10-05 23:55:32 +0800

    客厅相关页面

--- src/main/java/com/angelachen/myhome/controller/HtmlController.java
+++ src/main/java/com/angelachen/myhome/controller/HtmlController.java
@@ -1,7 +1,11 @@
 package com.angelachen.myhome.controller;
 
+import com.angelachen.myhome.common.model.User;
+import com.angelachen.myhome.common.util.UserUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Kartist 2018/9/21 22:39
@@ -19,6 +23,13 @@ public class HtmlController {
     public String commentHtml() {
 
         return "comment/index";
+    }
+
+    @GetMapping("/home/index")
+    public String livingRoom(HttpServletRequest request) {
+        User user = UserUtil.getGoodUser(request);
+        request.setAttribute("name", user.getName());
+        return "livingRoom";
     }
 
 }
